2c2d457c32caa2526bcec11cb1c6128fa4912c50
jeddaa <fathiato@gmail.com>
2023-05-25 15:19:44 +0100

    refactoring code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {
+  Routes, Route,
+  BrowserRouter,
+} from 'react-router-dom';
+import CountryList from './components/CountryList';
+import CountryItem from './components/CountryItem';
+import Navbar from './components/NavBar';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="w-11/12 my-0 mx-auto">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<CountryList />} />
+          <Route path="/:country" element={<CountryItem />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
